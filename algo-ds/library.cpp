29e2dbc972d2ffdf193f8cc64f8a6a7a41f0fdc2
Tristan Engen <jinine@gmail.com>
2024-11-10 16:04:24 -0800

    add member class

--- algo-ds/library.cpp
+++ algo-ds/library.cpp
@@ -3,6 +3,9 @@
 #include <list>
 #include <iterator>
 
+int bookID = 0;
+int memberID = 0;
+
 class Book
 {
 public:
@@ -10,12 +13,27 @@ public:
     std::string title;
     std::vector<std::pair<int, std::string>> content;
     int last_page = 0;
-
-    Book(int _id, std::string _title, std::vector<std::pair<int, std::string>> _content)
+    bool available = true;
+    Book(std::string _title, std::vector<std::pair<int, std::string>> _content)
     {
-        id = _id;
+        id = bookID;
         title = _title;
         content = _content;
+        bookID = bookID + 1;
+    }
+};
+
+class Member
+{
+public:
+    int id;
+    std::string name;
+    std::list<Book> checked_out = {};
+    Member(std::string _name)
+    {
+        id = memberID;
+        name = _name;
+        memberID = memberID + 1;
     }
 };
 
@@ -23,15 +41,25 @@ class Library
 {
 public:
     std::list<Book> book_list;
+    std::list<Member> member_list;
     Library()
     {
         book_list = {};
+        member_list = {};
     }
     void print_library()
     {
         for (auto it = book_list.begin(); it != book_list.end(); ++it)
         {
-            std::cout << it->title << '\n';
+            std::cout << it->id << '\n';
+        }
+    }
+
+    void print_members()
+    {
+        for (auto it = member_list.begin(); it != member_list.end(); ++it)
+        {
+            std::cout << it->name << '\n';
         }
     }
 
@@ -39,14 +67,34 @@ public:
     {
         book_list.insert(book_list.end(), book);
     }
+
+    void add_member(Member member)
+    {
+        member_list.insert(member_list.end(), member);
+    }
+
+    void check_out(Book book, Member member)
+    {
+    }
 };
 
 int main()
 {
     Library *library = new Library();
-    Book *book = new Book(0, "Title", {{0, "page 1"}});
+    Book *book = new Book("Title", {{0, "page 1"}});
+    Book *book_2 = new Book("Title", {{0, "page 1"}});
+    Book *book_3 = new Book("Title", {{0, "page 1"}});
+    Book *book_4 = new Book("Title", {{0, "page 1"}});
+    Book *book_5 = new Book("Title", {{0, "page 1"}});
     library->add_book(*book);
+    library->add_book(*book_2);
+    library->add_book(*book_3);
+    library->add_book(*book_4);
+    library->add_book(*book_5);
     library->print_library();
+    Member *member = new Member("Member");
+    library->add_member(*member);
+    library->print_members();
 
     return 0;
 }
